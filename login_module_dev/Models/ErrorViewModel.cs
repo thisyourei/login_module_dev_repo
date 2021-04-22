6fd14a6e1b2a6807d4f5b07f0c4453eaa4b2fbb5
thisyourei <60913697+thisyourei@users.noreply.github.com>
2021-04-22 13:39:17 -0400

    Add files
    se agregan los archivos iniciales del proyecto

---
+++ login_module_dev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace login_module_dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
